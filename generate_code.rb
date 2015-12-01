6c0cc02a999ea08cda5ecf6506807e3970872556
Pral2a <guillem@pral2a.com>
2015-12-01 12:21:14 +1100

    Updated with production end-point

--- generate_code.rb
+++ generate_code.rb
@@ -6,11 +6,11 @@ if File.exists?(filename)
   puts File.read(filename)
 else
   lines = ["> ### Example"]
-  lines << "#{method} https://new-api.smartcitizen.me/v0/#{ARGV[0]}"
+  lines << "#{method} https://api.smartcitizen.me/v0/#{ARGV[0]}"
   lines << ""
   lines << "```shell"
-  lines << `http #{method} 'https://new-api.smartcitizen.me/v0/#{ARGV[0]}' -p h`.split("\n")[0]
-  result = JSON.parse(`http #{method} 'https://new-api.smartcitizen.me/v0/#{ARGV[0]}#{ARGV[0].include?('?') ? '&' : '?'}pretty=true'`)
+  lines << `http #{method} 'https://api.smartcitizen.me/v0/#{ARGV[0]}' -p h`.split("\n")[0]
+  result = JSON.parse(`http #{method} 'https://api.smartcitizen.me/v0/#{ARGV[0]}#{ARGV[0].include?('?') ? '&' : '?'}pretty=true'`)
   if result.kind_of?(Array)
     lines << "["
     lines << JSON.pretty_generate( result[0] )
