467e4f55db501f34aa29ac96c1a008e7e5b4351d
Viktor Smari <viktorsmari@gmail.com>
2019-01-23 13:15:41 +0100

    Try auto deploy

---
+++ .travis-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+# setup ssh-agent and provide the GitHub deploy key
+eval "$(ssh-agent -s)"
+chmod 600 deploy_rsa
+ssh-add deploy_rsa
+
+# This method depends on how you build your project.
+# This is a node.js example:
+# commit the assets in build/ to the gh-pages branch and push to GitHub using SSH
+./deploy.sh
+
